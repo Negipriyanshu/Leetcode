4143053c9719ba2aa24e366fc23d85c2a017ee39
priyanshu negi <155433772+Negipriyanshu@users.noreply.github.com>
2025-02-08 02:25:37 +0530

    Time: 98 ms (69.44%), Space: 63.3 MB (33.33%) - LeetHub

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def queryResults(self, limit, queries):
+        ball = {}
+        color= {}
+        ans= []
+        distinct= 0
+        for pos, c in queries:
+            if pos in ball:
+                color[ball[pos]] -= 1
+                if color[ball[pos]] == 0:
+                    distinct -= 1
+            ball[pos] = c
+            color[c] = color.get(c, 0) + 1
+            if color[c] == 1: distinct += 1
+            ans.append(distinct)
+        return ans
+        
